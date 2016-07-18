574d4679e805179d69da11419588447fae9cae3c
David Bracewell <david@my_email.com>
2016-07-17 20:43:47 -0500

    Updates

---
+++ src/main/java/com/davidbracewell/stream/accumulator/StatisticsAccumulatable.java
@@ -0,0 +1,32 @@
+package com.davidbracewell.stream.accumulator;
+
+import com.davidbracewell.collection.EnhancedDoubleStatistics;
+
+/**
+ * @author David B. Bracewell
+ */
+public class StatisticsAccumulatable implements Accumulatable<EnhancedDoubleStatistics> {
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  public EnhancedDoubleStatistics addAccumulator(EnhancedDoubleStatistics t1, EnhancedDoubleStatistics t2) {
+    EnhancedDoubleStatistics eds = new EnhancedDoubleStatistics();
+    eds.combine(t1);
+    eds.combine(t2);
+    return eds;
+  }
+
+  @Override
+  public EnhancedDoubleStatistics addInPlace(EnhancedDoubleStatistics t1, EnhancedDoubleStatistics t2) {
+    t1.combine(t2);
+    return t1;
+  }
+
+  @Override
+  public EnhancedDoubleStatistics zero(EnhancedDoubleStatistics zeroValue) {
+    if( zeroValue == null ){
+      return new EnhancedDoubleStatistics();
+    }
+    return zeroValue;
+  }
+}// END OF StatisticsAccumulatable
