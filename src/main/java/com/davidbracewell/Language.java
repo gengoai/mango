35c34093b953f2304a7d7d1564ffa2c8c1843458
David Bracewell <david@my_email.com>
2017-05-08 14:28:49 -0500

    cleanup

--- src/main/java/com/davidbracewell/Language.java
+++ src/main/java/com/davidbracewell/Language.java
@@ -1,22 +1,22 @@
 /*
  * (c) 2005 David B. Bracewell
  *
- * Licensed to Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See NOTICE file
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership.  ASF licenses this file
- * to you under Apache License, Version 2.0 (the
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- * with License.  You may obtain a copy of License at
+ * with the License.  You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
- * software distributed under License is distributed on an
+ * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See License for the
+ * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under License.
+ * under the License.
  */
 
 package com.davidbracewell;
