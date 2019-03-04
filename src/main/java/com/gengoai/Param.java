4e2ea913f96e5092bd69656ecfaa31512d97a8ca
David Bracewell <david@davidbracewell.com>
2019-03-04 13:44:21 -0600

    Parameter Map for named parameters

---
+++ src/main/java/com/gengoai/Param.java
@@ -0,0 +1,164 @@
+/*
+ * (c) 2005 David B. Bracewell
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+package com.gengoai;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * The type Param.
+ *
+ * @param <T> the type parameter
+ * @author David B. Bracewell
+ */
+public class Param<T> implements Serializable {
+   private static final long serialVersionUID = 1L;
+   /**
+    * The Name.
+    */
+   public final String name;
+   /**
+    * The Type.
+    */
+   public final Class<T> type;
+
+
+   /**
+    * Instantiates a new Param.
+    *
+    * @param name the name
+    * @param type the type
+    */
+   public Param(String name, Class<T> type) {
+      this.name = name;
+      this.type = type;
+   }
+
+   /**
+    * Bool param param.
+    *
+    * @param name the name
+    * @return the param
+    */
+   public static Param<Boolean> boolParam(String name) {
+      return new Param<>(name, Boolean.class);
+   }
+
+   /**
+    * Double param param.
+    *
+    * @param name the name
+    * @return the param
+    */
+   public static Param<Double> doubleParam(String name) {
+      return new Param<>(name, Double.class);
+   }
+
+   /**
+    * Float param param.
+    *
+    * @param name the name
+    * @return the param
+    */
+   public static Param<Float> floatParam(String name) {
+      return new Param<>(name, Float.class);
+   }
+
+   /**
+    * Int param param.
+    *
+    * @param name the name
+    * @return the param
+    */
+   public static Param<Integer> intParam(String name) {
+      return new Param<>(name, Integer.class);
+   }
+
+   /**
+    * Long param param.
+    *
+    * @param name the name
+    * @return the param
+    */
+   public static Param<Long> longParam(String name) {
+      return new Param<>(name, Long.class);
+   }
+
+   /**
+    * Str param param.
+    *
+    * @param name the name
+    * @return the param
+    */
+   public static Param<String> strParam(String name) {
+      return new Param<>(name, String.class);
+   }
+
+   /**
+    * Check type.
+    *
+    * @param type the type
+    */
+   public void checkType(Class<?> type) {
+      if (!this.type.isAssignableFrom(type)) {
+         throw new IllegalArgumentException(
+            "Invalid type: " + type.getSimpleName() + ", expecting " + this.type.getSimpleName()
+         );
+      }
+   }
+
+   /**
+    * Check value.
+    *
+    * @param value the value
+    */
+   public void checkValue(Object value) {
+      if (!type.isInstance(value)) {
+         throw new IllegalArgumentException(
+            "Invalid value: " + value + ", expecting " + type.getSimpleName()
+         );
+      }
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (!(o instanceof Param)) return false;
+      Param<?> param = (Param<?>) o;
+      return Objects.equals(name, param.name) &&
+                Objects.equals(type, param.type);
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(name, type);
+   }
+
+   @Override
+   public String toString() {
+      return "Param{" +
+                "name='" + name + '\'' +
+                ", type=" + type +
+                '}';
+   }
+}//END OF Param
---
+++ src/main/java/com/gengoai/ParamMap.java
@@ -0,0 +1,217 @@
+/*
+ * (c) 2005 David B. Bracewell
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+package com.gengoai;
+
+import com.gengoai.conversion.Cast;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+/**
+ * The type Param map.
+ *
+ * @param <V> the type parameter
+ * @author David B. Bracewell
+ */
+public class ParamMap<V extends ParamMap> implements Serializable, Copyable<ParamMap<V>> {
+   private static final long serialVersionUID = 1L;
+   private final Map<String, Parameter<?>> map = new HashMap<>();
+
+   @Override
+   public ParamMap<V> copy() {
+      return Copyable.deepCopy(this);
+   }
+
+
+   /**
+    * Get t.
+    *
+    * @param <T>   the type parameter
+    * @param param the param
+    * @return the t
+    */
+   public <T> T get(Param<T> param) {
+      if (map.containsKey(param.name)) {
+         Parameter<?> parameter = map.get(param.name);
+         parameter.param.checkType(param.type);
+         return Cast.as(parameter.value);
+      }
+      throw new IllegalArgumentException("Unknown Parameter: " + param.name);
+   }
+
+   /**
+    * Get t.
+    *
+    * @param <T>   the type parameter
+    * @param param the param
+    * @return the t
+    */
+   public <T> T get(String param) {
+      if (map.containsKey(param)) {
+         return Cast.as(map.get(param).value);
+      }
+      throw new IllegalArgumentException("Unknown Parameter: " + param);
+   }
+
+   /**
+    * Gets or default.
+    *
+    * @param <T>          the type parameter
+    * @param param        the param
+    * @param defaultValue the default value
+    * @return the or default
+    */
+   public <T> T getOrDefault(Param<T> param, T defaultValue) {
+      if (map.containsKey(param.name)) {
+         return get(param);
+      }
+      return defaultValue;
+   }
+
+   /**
+    * Gets or default.
+    *
+    * @param <T>          the type parameter
+    * @param param        the param
+    * @param defaultValue the default value
+    * @return the or default
+    */
+   public <T> T getOrDefault(String param, T defaultValue) {
+      if (map.containsKey(param)) {
+         Parameter<?> parameter = map.get(param);
+         parameter.param.checkValue(defaultValue);
+         return Cast.as(parameter.value);
+      }
+      return defaultValue;
+   }
+
+   /**
+    * Parameter parameter.
+    *
+    * @param <T>      the type parameter
+    * @param param    the param
+    * @param defValue the def value
+    * @return the parameter
+    */
+   public <T> Parameter<T> parameter(Param<T> param, T defValue) {
+      return new Parameter<>(param, defValue);
+   }
+
+   /**
+    * Set v.
+    *
+    * @param <T>   the type parameter
+    * @param param the param
+    * @param value the value
+    * @return the v
+    */
+   public <T> V set(Param<T> param, T value) {
+      if (map.containsKey(param.name)) {
+         Parameter<?> parameter = map.get(param.name);
+         parameter.param.checkType(param.type);
+         parameter.value = Cast.as(value);
+         return Cast.as(this);
+      }
+      throw new IllegalArgumentException("Unknown Parameter: " + param.name);
+   }
+
+   /**
+    * Set v.
+    *
+    * @param <T>   the type parameter
+    * @param param the param
+    * @param value the value
+    * @return the v
+    */
+   public <T> V set(String param, T value) {
+      if (map.containsKey(param)) {
+         Parameter<?> parameter = map.get(param);
+         parameter.param.checkValue(value);
+         parameter.value = Cast.as(value);
+         return Cast.as(this);
+      }
+      throw new IllegalArgumentException("Unknown Parameter: " + param);
+   }
+
+   @Override
+   public String toString() {
+      return "ParamMap{" + map.values()
+                              .stream()
+                              .map(p -> p.param.name + "=" + p.value)
+                              .collect(Collectors.joining(", ")) + "}";
+   }
+
+   /**
+    * Update v.
+    *
+    * @param updater the updater
+    * @return the v
+    */
+   public V update(Consumer<V> updater) {
+      updater.accept(Cast.as(this));
+      return Cast.as(this);
+   }
+
+   /**
+    * The type Parameter.
+    *
+    * @param <T> the type parameter
+    */
+   public class Parameter<T> implements Serializable {
+      private static final long serialVersionUID = 1L;
+      /**
+       * The Param.
+       */
+      public final Param<T> param;
+      /**
+       * The Value.
+       */
+      private T value;
+
+      /**
+       * Instantiates a new Parameter.
+       *
+       * @param param        the param
+       * @param defaultValue the default value
+       */
+      private Parameter(Param<T> param, T defaultValue) {
+         this.param = param;
+         map.put(param.name, this);
+         this.value = defaultValue;
+      }
+
+      public V set(T value) {
+         this.value = value;
+         return Cast.as(ParamMap.this);
+      }
+
+      public T value() {
+         return value;
+      }
+   }
+
+
+}//END OF ParamMap
